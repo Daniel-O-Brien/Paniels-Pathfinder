6c02316a4578ba024131636f234638eb8eb1b02d
Daniel <dobrien@atenik.xyz>
2024-05-12 15:26:12 +0100

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class Main {
+
+    @FXML
+    private void closeHand(){
+    }
+
+    @FXML
+    private void openHand(){
+    }
+
+    @FXML
+    private void selectLocation(){
+    }
+
+}
