4f446494c4763c8a8f246a24750d13f5eb55c038
Patrick Kiersey <Patrick.Kiersey2.0@gmail.com>
2024-05-12 16:29:29 +0100

    Commit damn you!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,70 @@
 package org.example;
 
 import java.io.IOException;
+import java.io.Reader;
+
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Slider;
+import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class Main {
 
+    public ImageView mapView;
+    public Slider zoom;
+
+    private Image map = mapView.getImage();
+    private PixelReader mapReader = map.getPixelReader();
+    private WritableImage writableImage;
+    private PixelWriter writer;
+
+
+    public void initialize()
+    {
+        this.writableImage = new WritableImage((int) this.map.getWidth(), (int) this.map.getHeight());
+        writer = writableImage.getPixelWriter();
+    }
+
+
+
+
     @FXML
-    private void closeHand(){
+    private void closeHand()
+    {
+        zoom.setCursor(Cursor.CLOSED_HAND);
     }
 
     @FXML
     private void openHand(){
+        zoom.setCursor(Cursor.OPEN_HAND);
     }
 
     @FXML
-    private void selectLocation(){
+    private void selectLocation(MouseEvent mouseEvent){
+        //Initialise coordinates.
+        double x, y;
+
+        //Setting coordinates.
+        x = mouseEvent.getX();
+        y = mouseEvent.getY();
+
+        System.out.println("You have clicked: " + x + "," + y);
+
+        for(double j = (y - 30); j < (y + 30); j++)
+        {
+
+            for (double i = (x - 30); i < (x + 30); i++)
+            {
+                this.writer.setColor( (int) j, (int) i, Color.RED);
+                System.out.println(i + "," + j);
+            }
+
+            mapView.setImage(this.writableImage);
+        }
+        /**Figure out how to set the map to the writable image */
+
     }
 
 }
